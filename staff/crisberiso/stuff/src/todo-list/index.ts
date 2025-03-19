3006ceea31b1401556ba4f8fcddc6ac5d0b2f4b7
CriBeriso <berith93@icloud.com>
2025-03-19 20:57:24 +0100

    add to do list miniproject #14

---
+++ staff/crisberiso/stuff/src/todo-list/index.ts
@@ -0,0 +1,69 @@
+const input = document.getElementById("todo-text")! as HTMLInputElement; //definimos que no es null y el tipo de elemento html que es. Ahora si podemos acceder al valor del input.
+const form = document.querySelector(".todo-form")! as HTMLFormElement;
+const ul = document.querySelector(".todo-list")! as HTMLUListElement;
+
+type Todo = {
+  text: string;
+  isCompleted: boolean;
+}
+
+const todos: Todo[] = getTodos();
+
+todos.forEach(renderTodo);
+
+form.addEventListener("submit", (event: SubmitEvent) => {
+  event.preventDefault();
+
+  const { value: todoText } = input;
+
+  try {
+    const todo = createTodo(todoText);
+
+    //TODO separar capas de responsabilidad. 
+    todos.push(todo);
+
+    saveTodos();
+
+    form.reset();
+
+    renderTodo(todo);
+  } catch (error) {
+    alert((error as Error).message);
+    console.error((error as Error).message);
+  }
+  
+});
+
+function createTodo(text: string): Todo {
+  if (!text) throw new Error('Input text is empty!');
+  return {
+    text,
+    isCompleted: false,
+  };
+}
+
+function renderTodo(todo: Todo) {
+  const li = document.createElement("li");
+  li.textContent = todo.text;
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = todo.isCompleted; 
+
+  checkbox.addEventListener("change", () => {
+    todo.isCompleted = checkbox.checked;
+
+    saveTodos();
+  })
+  
+  li.appendChild(checkbox);
+  ul.appendChild(li);
+}
+
+function saveTodos():void {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getTodos(): Todo[] {
+  return JSON.parse(localStorage.getItem("todos") || "[]")
+}
